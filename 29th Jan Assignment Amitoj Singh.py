53660f5b02661b7d00beea1c2df0c170c8cb23a6
Amitojsingh13 <122732225+Amitojsingh13@users.noreply.github.com>
2023-03-06 14:36:05 +0530

    Add files via upload

---
+++ 29th Jan Assignment Amitoj Singh.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Amitoj Singh
+
+# # Q1
+# Guido van Rossum developed python programming language
+
+# # Q2
+# Object-oriented programming and structured programming are supported by Python
+
+# # Q3
+# Yes it is case sensitive because the identifier Name and name are two different here. 
+
+# # Q4
+# Correct extension for python is ‘.py’
+
+# # Q5
+# It is first compiled then it is interpreted. It is done line by line.
+
+# # Q6
+# If, Else, While and For
+# 
+
+# # Q7
+# Hash# is the character used for single line comment
+# 
+
+# # Q8
+# import sys
+# 
+# print(sys.version)
+
+# In[1]:
+
+
+import sys
+
+print(sys.version)
+
+
+# # Q9
+# Lambda Function
+# 
+
+# # Q10
+# pip stands for "Pip installs packages"
+
+# # Q11
+# print() - prints the specified message to the screen.
+# len() - returns the number of items in an object (such as a string, list, or tuple).
+# type() - returns the type of an object.
+# int() - converts a string or float to an integer.
+# float() - converts a string or integer to a float.
+# str() - converts an object to a string.
+# list() - converts an object to a list.
+
+# # Q13
+# Advantages of using python are:
+# 1. Easy to use
+# 2. Built in Library 
+# 3. Open source and community driven
+
+# # Q14
+#  Python, memory management is handled automatically by a mechanism called the "Python memory manager". The memory manager is responsible for allocating memory for objects, tracking object references, and freeing memory when objects are no longer needed.
+# 
+# Python uses a technique called "reference counting" to keep track of object references. Each object in Python has a reference count, which is incremented whenever a new reference to the object is created, and decremented whenever a reference is deleted. When an object's reference count reaches zero, the memory used by the object is automatically freed.
+
+# # Q15
+# To install Python on Windows and set path variables, you can follow these steps:
+# 
+# 1. Download the Python installer from the official website (https://www.python.org/downloads/windows/).
+# 2. Run the installer and select "Install Now" when prompted.
+# 3. Follow the prompts in the installer to select the installation directory and choose the components to install.
+# 4. After the installation is complete, open the Start menu and search for "Environment Variables".
+# 5. Click on "Edit the system environment variables".
+# 6. Click on the "Environment Variables" button at the bottom of the System Properties window.
+# 7. In the "System variables" section, scroll down until you find the "Path" variable and click on "Edit".
+# 8. Click on "New" and add the path to your Python installation directory (e.g. C:\Python39).
+# 9. Click "OK" to save the changes and close all windows.
+# 10. After you have completed these steps, you should be able to run Python from the command line by typing "python" into the terminal. You can also run Python scripts by navigating to the directory containing the script and typing "python script_name.py" into the terminal.
+
+# # Q16
+# Yes indentations are required.Its very important we take care of indentations in python especially while using loops like 'for','if else','while'. Other wise error may be showed if we dont take care of indentations.
+
+# In[ ]:
+
+
+
+
